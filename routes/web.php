d7dbe0d4c5dc087289e1e4471dd549deb58f84e0
Shahid <shahid.siddique@iplex.co>
2024-06-12 20:25:36 +0500

    E-Comm site

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,17 @@
-
-
-
 <?php
 
-// use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
+Route::get('/main', [RegistrationController::class, 'index'])->name('main');
 
-// /*
-// |--------------------------------------------------------------------------
-// | Web Routes
-// |--------------------------------------------------------------------------
-// |
-// | Here is where you can register web routes for your application. These
-// | routes are loaded by the RouteServiceProvider within a group which
-// | contains the "web" middleware group. Now create something great!
-// |
-// */
+Route::redirect('/', '/main');
 
-Route::get('/main', [RegistrationController::class, 'index']);
-
-Route::get('/', function () {
-    return redirect('/main');
+Route::middleware(['auth'])->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    require __DIR__.'/auth.php';
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
- 
-Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
@@ -39,10 +19,6 @@ Route::get('/user/create', [UserController::class, 'create'])->name('user.create
 
 Route::get('/form', [RegistrationController::class, 'index'])->name('register123');
 
-
-
-
-
-
-
-
+Route::get('/Products', function () {
+    return view('login');
+});
